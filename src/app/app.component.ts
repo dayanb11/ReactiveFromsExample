09b5650ffc2d892fb8a722b9864f40dc57c4df09
dayanb11 <dayanbeni@gmail.com>
2023-11-24 21:02:55 +0200

    commit small commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,26 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  registrationForm = new FormGroup({
-    userName: new FormControl('Beni the king'),
-    password: new FormControl('123'),
-    confirmPassword: new FormControl(''),
-    address: new FormGroup({
-      city: new FormControl(''),
-      state: new FormControl('')
-    })
-  })
+export class AppComponent implements OnInit {
+  LoadApi():void
+{  alert('LoadApi');}
+  registrationForm: FormGroup;
+  constructor(private fb: FormBuilder,address:FormGroup) {
+    this.registrationForm = new FormGroup({
+      UserName: new FormControl('Beni the king'),
+      Password: new FormControl('123'),
+      ConfirmPassword: new FormControl('123')
+    }),
+      address = new FormGroup({
+        City: new FormControl('Los angeles'),
+        State: new FormControl('California')
+      })
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
